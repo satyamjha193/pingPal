a54df8a655667c297e4e4058245e247db7ab879b
Satyam Jha <jha876987@gmail.com>
2025-09-09 20:57:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,14 +22,25 @@ mongoose.connect(process.env.MONGO_URI, {})
 .then(() => console.log('✅ MongoDB connected'));
 
 // Session
+// MongoDB
+// mongoose.connect(process.env.MONGO_URI, {})
+// .then(() => console.log('✅ MongoDB connected'));
+
+// // Session
+// app.use(session({
+//   secret: process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: false,
+//   store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+//   cookie: { maxAge: 1000 * 60 * 60 * 24 } // 1 day
+// }));
+
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
-  cookie: { maxAge: 1000 * 60 * 60 * 24 } // 1 day
+  saveUninitialized: false
 }));
-
 
 
 
