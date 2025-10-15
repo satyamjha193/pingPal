45174fe32116636d01a90ab0e8dbf3b74d5a5b54
Satyam Jha <jha876987@gmail.com>
2025-10-15 13:58:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -111,12 +111,6 @@ app.get('/test-db', async (req, res) => {
   }
 });
 
-// ------------------- Start server -------------------
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
-
 
 // Start server
 const PORT = process.env.PORT || 3000;
